c25457e09db081e064bc0383edd56c0fad84907b
Matias A. Rossi <43966706+MatiDragon-YT@users.noreply.github.com>
2021-09-18 13:00:56 -0500

    Update header.js

--- js/partial/header.js
+++ js/partial/header.js
@@ -10,28 +10,21 @@ document.write('\
   <tbody>\
   <tr>\
     <td valign=top width="100%" background="../../img/en/logo1.jpg">\
-      <img\
-      src="../../img/en/logo2.jpg" width="640" height="121" border=0 class="conthdr" >\
+      <div style="\
+        background: url(\'../../img/en/logo2.jpg\') no-repeat;\
+        height: 121px;\
+      "></div>\
     </td>\
-  </tr></tbody></table>\
+  </tr>\
+  </tbody>\
+</table>\
 <table cellspacing=0 cellpadding=0 width="100%" border=0>\
   <tbody>\
   <td width="100%" height="100%" valign=top>\
-    <div style="\
-    background-image: linear-gradient(\
-      0deg, #6487bb 2px, #3c5177 2px, #7280a7 26px, #739dcb\
-    );\
-    color: white;\
-    font-weight: bold;\
-    font-style: italic;\
-    font-family: sans-serif;\
-    text-shadow: 6px 4px 8px black;\
-    box-shadow: inset 0px 0px 12px 7px #8483f77a, 2px 3px 3px 0px #d2d2d2;\
-    border-radius: 11px;\
-    margin-bottom: 10px;\
-">\
-    <div style="background: repeating-linear-gradient(136deg, #00006612 3px, #00006612 3px, transparent 6px, transparent 8px);padding: 8px 33px 0px;height: 1.6rem;">SCM Documentation</div>\
-</div>\
+    <div style="background-image: linear-gradient(      0deg, #6487bb 2px, #3c5177 2px, #7280a7 26px, #739dcb    );color: white;font-weight: bold;font-style: italic;font-family: sans-serif;text-shadow: 6px 4px 8px black;box-shadow: inset 0px 0px 12px 7px #8483f77a, 2px 3px 3px 0px #d2d2d2;border-radius: 11px;margin-bottom: 10px;">    <div style="background: repeating-linear-gradient(136deg, #00006612 3px, #00006612 3px, transparent 6px, transparent 8px);"><p style="\
+    padding: 8px 33px;\
+    margin: 0;\
+">SCM Documentation</p></div></div>\
       <table cellspacing=0 cellpadding=0 width="100%">\
         <tbody>\
         <tr>\
@@ -46,10 +39,11 @@ document.write('\
       width=20 style="\
     position: relative;\
     top: -1px;\
-"></td></tr></tbody></table>\
-      <table cellspacing=0 cellpadding=0 width="100%">\
-        <tbody>\
-        <tr>\
-          <td width=5 background="../../img/en/ltd.gif"></td>\
-          <td class=news valign=top>\
+"></td></tr></tbody>\
+</table>\
+<table cellspacing=0 cellpadding=0 width="100%">\
+  <tbody>\
+  <tr>\
+    <td width=5 background="../../img/en/ltd.gif"></td>\
+    <td class=news valign=top>\
 ')
