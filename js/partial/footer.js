ea5c3a0f6309c9e0d091dde09551a225f22faf09
Matias A. Rossi <43966706+MatiDragon-YT@users.noreply.github.com>
2021-09-17 21:02:37 -0500

    .htm -> .html

---
+++ js/partial/footer.js
@@ -0,0 +1,20 @@
+document.write('\
+    </td>\
+          <td width=5\
+        background="rtd.gif"></td></tr></tbody></table>\
+      <table cellspacing=0 cellpadding=0 width="100%">\
+        <tbody>\
+        <tr>\
+          <td width=5><img height=20\
+            src="img/en/mtdlbot.png" width=5 border=0></td>\
+          <td align=left\
+            background="mtdbgbot.png"></td>\
+          <td class=ninfo align=right\
+          background="mtdbgbot.png">&nbsp;\
+            </td>\
+          <td width=5><img height=20\
+            src="img/en/mtdrbot.png" width=5\
+        border=0></td></tr></tbody></table></div>\
+</td></tr><td width="100%"></tbody></table>\
+</table></body></html>\
+')
---
+++ js/partial/header.js
@@ -0,0 +1,32 @@
+document.querySelector("body").setAttribute("topMargin", 0)
+
+document.write('\
+<table\
+style="border-right: medium none; border-top: medium none; border-left: medium none; border-bottom: #c0c0c0 1px solid"\
+height=121 cellspacing=0 cellpadding=0 width=640>\
+  <tbody>\
+  <tr>\
+    <td valign=top width="100%"><img\
+      src="../../img/en/logo2.jpg" width="640" height="121" border=0 class="conthdr" ></td>\
+  </tr></tbody></table>\
+<table cellspacing=0 cellpadding=0 width=640 border=0>\
+  <tbody>\
+  <td width="100%" height="100%" valign=top>\
+   <div><img\
+      src="../../img/en/old/scm/articles.png" width="640" height="33"></div>\
+      <table cellspacing=0 cellpadding=0 width="100%">\
+        <tbody>\
+        <tr>\
+          <td width=5><img height=36\
+            src="../../img/en/mtdl.png" width=5></td>\
+          <td class=ntitle valign=top\
+          background="mtdbg.png" height=36>sanny builder help: '+document.title+'</td>\
+          <td width=5><img height=36\
+            src="../../img/en/mtdr.png"\
+      width=20></td></tr></tbody></table>\
+      <table cellspacing=0 cellpadding=0 width="100%">\
+        <tbody>\
+        <tr>\
+          <td width=5 background="ltd.gif"></td>\
+          <td class=news valign=top>\
+')
