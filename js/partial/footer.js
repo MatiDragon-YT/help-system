04f014ebedd6571220267330e870a66e49c0760d
Matias A. Rossi <43966706+MatiDragon-YT@users.noreply.github.com>
2021-09-18 11:42:28 -0500

    Update footer.js

--- js/partial/footer.js
+++ js/partial/footer.js
@@ -1,19 +1,19 @@
 document.write('\
     </td>\
           <td width=5\
-        background="rtd.gif"></td></tr></tbody></table>\
+        background="../../img/en/rtd.gif"></td></tr></tbody></table>\
       <table cellspacing=0 cellpadding=0 width="100%">\
         <tbody>\
         <tr>\
           <td width=5><img height=20\
-            src="img/en/mtdlbot.png" width=5 border=0></td>\
+            src="../../img/en/mtdlbot.png" width=5 border=0></td>\
           <td align=left\
-            background="mtdbgbot.png"></td>\
+            background="../../img/en/mtdbgbot.png"></td>\
           <td class=ninfo align=right\
-          background="mtdbgbot.png">&nbsp;\
+          background="../../img/en/mtdbgbot.png">&nbsp;\
             </td>\
           <td width=5><img height=20\
-            src="img/en/mtdrbot.png" width=5\
+            src="../../img/en/mtdrbot.png" width=5\
         border=0></td></tr></tbody></table></div>\
 </td></tr><td width="100%"></tbody></table>\
 </table></body></html>\
