76b5f1c88218f15c508c4032e861bffc79b593e5
Matias A. Rossi <43966706+MatiDragon-YT@users.noreply.github.com>
2021-09-23 12:26:39 -0500

    init minimal style

--- js/partial/header.js
+++ js/partial/header.js
@@ -5,22 +5,7 @@ function $(str){
   return document.querySelectorAll(str);
 }
 
-/** 
-*/
-function Title(){
-  const sb = 'Sanny Builder';
-  var msm = '';
-  switch ($('html')[0].lang || 'en') {
-    case 'es' : msm = 'Ayuda del '  +sb   ;break;
-    case 'pt' : msm = 'Ajuda do '   +sb   ;break;
-    case 'ru' : msm = 'Справка по ' +sb   ;break;
-    case 'en' :
-    default   : msm = sb + ' Help'        ;break;
-  }
-  return msm + ': ' + (document.title || "Topic")
-}
-
-function Direction(){
+function DIRECTION(){
   var defDirection = parseInt($('html')[0].getAttribute('cd')) || '../../';
 
   if (isNaN(defDirection)){
@@ -33,36 +18,15 @@ function Direction(){
   return newDirection
 }
 
-var $id = ($('html')[0].id || 'Documentation_SCM').replace(/(_)/, ' ');
-
+const ID = ($('html')[0].id || 'Documentation_SCM').replace(/(_)/, ' ');
+const TITLE = document.title || "Topic";
 document.write('\
-<table height=121 cellspacing=0 cellpadding=0 width="100%">\
-  <tbody>\
-    <tr>\
-      <td valign=top width="100%" background="'+ Direction() +'img/logo1.jpg">\
-        <div style="background: url(\''+ Direction() +'img/logo2.jpg\') no-repeat;height: 121px;"></div>\
-      </td>\
-    </tr>\
-  </tbody>\
-</table>\
-<table cellspacing=0 cellpadding=0 width="100%" border=0>\
-  <tbody>\
-  <td width="100%" height="100%" valign=top>\
-    <div style="background-image: url(\''+ Direction() +'img/var.png\');background-size: contain;color: white;font-weight: bold;font-style: italic;font-family: sans-serif;text-shadow: 6px 4px 8px black;box-shadow: inset 0px 0px 12px 7px #8483f77a, 2px 3px 3px 0px #d2d2d2;border-radius: 11px;margin-bottom: 10px;">\
-      <p style="padding: 8px 33px;margin: 0">'+ $id +'</p>\
-    </div>\
-    <table cellspacing=0 cellpadding=0 width="100%">\
-      <tbody>\
-        <tr>\
-          <td width=5><img height="36" src="'+ Direction() +'img/mtdl.png" width="5" style="position: relative;top: -1px;"></td>\
-          <td class=ntitle valign=top background="'+ Direction() +'img/mtdbg.png" height=36>'+ Title() +'</td>\
-          <td width=5><img height=36 src="'+ Direction() +'img/mtdr.png" width=20 style="position: relative;top: -1px;"></td>\
-        </tr>\
-      </tbody>\
-    </table>\
-    <table cellspacing=0 cellpadding=0 width="100%">\
-    <tbody>\
-    <tr>\
-      <td width=5 background="'+ Direction() +'img/ltd.gif"></td>\
-      <td class=news valign=top>\
+<div style="background: #263238 url(\''+ DIRECTION() +'img/logo1.png\')">\
+  <div style="background: #263238 url(\''+ DIRECTION() +'img/logo2.png\') no-repeat;height: 121px;width: 640px"></div>\
+</div>\
+<div style="background: #5a97f3;color: black;font-weight: bold;">\
+  <h2 style="padding: 9px 33px">'+ ID +'</h2>\
+</div>\
+<div style="padding: 0.67pc 2pc">\
+  <h2 style="font-weight: lighter;">'+ TITLE +'</h2>\
 ')
