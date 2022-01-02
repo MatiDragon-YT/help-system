cf6c0f7c5fc1587eac5998fec33f0b11ac5fa1e5
Matias A. Rossi <43966706+MatiDragon-YT@users.noreply.github.com>
2022-01-02 12:28:39 -0300

    🟢 Added file for test Markdown

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 // GLOBAL VERSION OF THE CHM
 const VERSION = "1.8";
 
+function log(value){
+	console.log(value)
+}
+
 // GLOBAL VARS
 var t="";
 const EMOJIS = {
 	'clap': '👏',
+	'wave' : '👋',
 	'+1' : '👍',
-	'-1' : '👎'
+	'-1' : '👎',
+	'smile' : '😄',
+	'eyes' : '👀'
 }
 const D = document
 
@@ -48,171 +55,176 @@ function apply(element, callback){
 	}
 }
 
+function textToMarkdown(str){
+	return str
+
+	/******** LIST ********/
+
+	// UL LI
+	.replace(/^\*\s(.+)/gim, '<ul><li>$1</li></ul>')
+	.replace(/^\x20{2}\*\s(.+)/gim, '<ul><ul><li>$1</li></ul></ul>')
+	.replace(/^\x20{4}\*\s(.+)/gim, '<ul><ul><ul><li>$1</li></ul></ul></ul>')
+	.replace(/<\/ul>(\s+)<ul>/g, '')
+	.replace(/<\/ul><\/ul><ul><ul>/g, '')
+	.replace(/<\/ul><ul>/g, '')
+
+	// OL LI
+	.replace(/^\d\.\s(.+)/gim, '<ol><li>$1</li></ol>')
+	.replace(/^\x20{2}\d\.\s(.+)/gim, '<ol><ol><li>$1</li></ol></ol>')
+
+	.replace(/<\/li><\/ol>\n<ul><ul><li>(.*)<\/ul><\/ul>/g, '<\/li><ul><li>$1</ul></ol>')
+
+	.replace(/<\/ol>(\s+)<ol>/g, '')
+	.replace(/<\/ol><ol>/g, '')
+
+	// DL DD
+	.replace(/^\-\s(.+)/gim, '<dl><dd>$1</dd></dl>')
+	.replace(/^\x20{2}\-\s(.+)/gim, '<dl><dl><dd>$1</dd></dl></dl>')
+	.replace(/<\/dl>(\s+)<dl>/g, '')
+	.replace(/<\/dl><dl>/g, '')
+
+	/*** FORMAT ***/
+	.replace(/(\s|\(|>)\*\*\*([\x20-\x29\x2B-\uFFFF]+)\*\*\*/g, '$1<b><i>$2</i></b>')
+	.replace(/(\s|\(|>)\*\*([\x20-\x29\x2B-\uFFFF]+)\*\*/g, '$1<b>$2</b>')
+	.replace(/(\s|\(|>)\*([\x20-\x29\x2B-\uFFFF]+)\*/g, '$1<i>$2</i>')
+	.replace(/(\s|\(|>)~~([\x20-\x7D\xA0-\uFFFF]+)~~/g, '$1<s>$2</s>')
+	.replace(/(\s|\(|>)__([\x20-\x5E\x60-\uFFFF]+)__/g, '$1<u>$2</u>')
+	.replace(/(\s|\(|>)==([\x20-\x3C\x3E-\uFFFF]+)==/g, '$1<mark>$2</mark>')
+	.replace(/(\s|\(|>)\+\+([\x20-\x2A\x2C-\uFFFF]+)\+\+/g, '$1<ins>$2</ins>')
+
+	// EMOJIS
+	.replace(/(\B|\s+):([\x21-\x39\x3B-\uFFFF]+):(\B)/g, function(input){
+
+		input = input.split(':')
+
+		return input[0] + EMOJIS[input[1]] + input[2]
+	})
+
+	/*** DIVS ***/
+	.replace(/{% hint (\w+) %}([\w\W]*){% endhint %}/g, "<div class='$1'>$2</div>")
+	.replace(/{% hint style="(\w+)" %}|:::([\w\d\x20-]+)\n/g, '<div class="$1$2">')
+	.replace(/{% endhint %}|:::\n/g, '</div>\n')
+	//.replace(/:::([\w\d\x20-]+)\n([\x09-\x39\x3B-\uFFFF]+):::/gim, '<div class=$1>$2</div>')
+
+	/*** BLOCKQUOTE ***/
+	.replace(/^>\x20(.+)/gim, '<blockquote>$1</blockquote>')
+	.replace(/<\/blockquote>(\s+)<blockquote>/g, '<br>')
+
+	/*** TITLE ***/
+
+	.replace(/^(\w.+)\n==+=\n/gim, function(input){
+		input = input.trim().replace(/^(\w.+)\n==+=/gim, '$1')
+		return '<h1 id="'+ input.toLinkCase() +'">' + input + '</h1>'
+	})
+	.replace(/^(\w.+)\n--+-\n/gim, function(input){
+		input = input.trim().replace(/^(\w.+)\n--+-/gim, '$1')
+		return '<h2 id="'+ input.toLinkCase() +'">' + input + '</h2>'
+	})
+	.replace(/^#+\x20(.+)/gm, function(input) {
+		var number = 0
+		var output
+
+		function index(text) {
+
+			if(/^#+\x20/.test(text)){
+				number++
+				text = text.replace(/^#/, '')
+				index(text, number)
+			}else{
+				if (number == 0){
+					output = text
+					return;
+				}
+				const TITLE = text.replace(/^\x20/, '')
+				output = '<h'+number+' id="'+ TITLE.toLinkCase() +'">' + TITLE + '</h'+number+'>'
+			}
+		}
+		index(input)
+
+		return output
+	})
+
+	// IMG
+	.replace(/\!\[([\x20-\x5A\x5C\x5E-\uFFFF]+)?\]\(([\x20-\x27\x2A-\uFFFF]+)(\s"[\w\d\s].+")?\)/g, '<img src="$2" alt="$1" title="$3">')
+
+	// A
+	.replace(
+		/\[([\x20-\x5A\x5C\x5E-\uFFFF]+)\]\(([\x21-\x27\x2A-\uFFFF]+)(\x20"[\w\d\s]+")?\)/g, function(input){
+			
+		var display = input.match(/\[(.+)\]/)[1]
+		var href   =   ' href="' + input.match(/\((.+)\)/)[1].replace(/\x20"(.+)"/, '') + '"'
+		var title  =  ' title="' + getTitle() + '"'
+		var target = ' target="'
+
+		if(/http/.test(href)){
+			target += '_blank"'
+		}else{
+			target += '_self"'
+			href = href.replace(/\.md/, '.html')
+		}
+		
+		function getTitle(){
+			if(/"(.+)"/.test(input)){
+				return input.match(/"(.+)"/)[1]
+			}
+			return ''
+		}
+
+		return '<a'+ href + title + target + '>' + display + '</a>'
+	})
+
+	// HR
+	.replace(/(\n|^)--+-\n/g, '$1<hr>\n')
+
+	// BR
+	.replace(/(\n^\.\n|(\.|:|\!|\))\n\n(\w|\d|<(ul|ol)?!|\*|`(``)?!))/g, '$2<br><br>$3')
+	.replace(/(\\\n|\\n\w|(\.|:|\!|\))\n(\w|\d|<(ul|ol)?!|\*|`(``)?!))/g, '$2<br>$3')
+
+	// PRE
+	.replace(/```([\x09-\x5F\x61-\uFFFF]*)```/g, function(input){
+
+		var display = input
+			.replace(/```(\w+)?\n([\x09-\x5F\x61-\uFFFF]*)```/, '$2').parseHTML()
+
+		function getLang(){
+			if(/```(\w+)\n/.test(input)){
+				return input.match(/```(\w+)\n/)[1]
+			}
+			return ''
+		}
+
+		return '<pre class="' + getLang() + '">' + display + '</pre>'
+	})
+
+	// CODE
+	.replace(/`([\x20-\x5F\x61-\uFFFF]+)`/g, function(input){
+		input = input.replace(/`([\x20-\x5F\x61-\uFFFF]+)`/, '$1').parseHTML()
+
+		return '<code>' + input + '</code>'
+	})
+	// SPAN
+	.replace(/\[([\w\d\-\x20]+)\]\[([\x20-\x57\x59\x6B-\uFFFF]+)\]/gim, '<span class="$1">$2</span>')
+}
+
 $('body').innerHTML = '\
 <div id="navbar" style="background: #5a97f3;color: black;font-weight: bold;overflow: hidden;">\
 	<h1 style="padding: 9px 33px;border-bottom: none;margin-bottom: 0;">'+ D.title +'</h1>\
 </div>\
-<textarea id="inputText" style="display:none;">'
+<div id="c"><div id="d"><textarea id="inputText" style="display:none;" disabled>'
 + $('body').innerHTML +
-'</textarea>\
-<div class="markdown"></div>'
+'</textarea></div>\
+<div class="markdown">\
+	<div class="cont"></div>\
+	<hr>\
+	<p style="line-height: 22px;font-weight: 500;font-size: 14px; color:#8899a8;">\
+		CHM ' + LANG + ' ' + VERSION + ' - Mada with <3 by MatiDragon, Seemann & Yushae Raza.\
+	</p>\
+	<span id="alinks"></span>\
+</div></div>'
 
-$('#inputText').value = $('#inputText').value
 
-/******** LIST ********/
 
-// UL LI
-.replace(/^\*\s(.+)/gim, '<ul><li>$1</li></ul>')
-.replace(/^\x20{2}\*\s(.+)/gim, '<ul><ul><li>$1</li></ul></ul>')
-.replace(/^\x20{4}\*\s(.+)/gim, '<ul><ul><ul><li>$1</li></ul></ul></ul>')
-.replace(/<\/ul>(\s+)<ul>/g, '')
-.replace(/<\/ul><\/ul><ul><ul>/g, '')
-.replace(/<\/ul><ul>/g, '')
-
-// OL LI
-.replace(/^\d\.\s(.+)/gim, '<ol><li>$1</li></ol>')
-.replace(/^\x20{2}\d\.\s(.+)/gim, '<ol><ol><li>$1</li></ol></ol>')
-
-.replace(/<\/li><\/ol>\n<ul><ul><li>(.*)<\/ul><\/ul>/g, '<\/li><ul><li>$1</ul></ol>')
-
-.replace(/<\/ol>(\s+)<ol>/g, '')
-.replace(/<\/ol><ol>/g, '')
-
-// DL DD
-.replace(/^\-\s(.+)/gim, '<dl><dd>$1</dd></dl>')
-.replace(/^\x20{2}\-\s(.+)/gim, '<dl><dl><dd>$1</dd></dl></dl>')
-.replace(/<\/dl>(\s+)<dl>/g, '')
-.replace(/<\/dl><dl>/g, '')
-
-/*** FORMAT ***/
-.replace(/(\s|\(|>)\*\*\*([\x20-\x29\x2B-\xFF]+)\*\*\*/g, '$1<b><i>$2</i></b>')
-.replace(/(\s|\(|>)\*\*([\x20-\x29\x2B-\xFF]+)\*\*/g, '$1<b>$2</b>')
-.replace(/(\s|\(|>)\*([\x20-\x29\x2B-\xFF]+)\*/g, '$1<i>$2</i>')
-.replace(/(\s|\(|>)~~([\x20-\x7D\xA0-\xFF]+)~~/g, '$1<s>$2</s>')
-.replace(/(\s|\(|>)__([\x20-\x5E\x60-\xFF]+)__/g, '$1<u>$2</u>')
-.replace(/(\s|\(|>)==([\x20-\x3C\x3E-\xFF]+)==/g, '$1<mark>$2</mark>')
-.replace(/(\s|\(|>)\+\+([\x20-\x2A\x2C-\xFF]+)\+\+/g, '$1<ins>$2</ins>')
-
-// EMOJIS
-.replace(/(\B|\s+):([\x21-\x39\x3B-\xFF]+):(\B)/g, function(input){
-
-	input = input.split(':')
-
-	return input[0] + EMOJIS[input[1]] + input[2]
-})
-
-/*** DIVS ***/
-.replace(/{% hint (\w+) %}([\w\W]*){% endhint %}/g, "<div class='$1'>$2</div>")
-.replace(/{% hint style="(\w+)" %}|:::([\w\d\x20-]+)\n/g, '<div class="$1$2">')
-.replace(/{% endhint %}|:::\n/g, '</div>\n')
-//.replace(/:::([\w\d\x20-]+)\n([\x09-\x39\x3B-\xFF]+):::/gim, '<div class=$1>$2</div>')
-
-/*** BLOCKQUOTE ***/
-.replace(/^>\x20(.+)/gim, '<blockquote>$1</blockquote>')
-.replace(/<\/blockquote>(\s+)<blockquote>/g, '<br>')
-
-/*** TITLE ***/
-
-.replace(/^(\w.+)\n==+=\n/gim, function(input){
-	input = input.trim().replace(/^(\w.+)\n==+=/gim, '$1')
-	return '<h1 id="'+ input.toLinkCase() +'">' + input + '</h1>'
-})
-.replace(/^(\w.+)\n--+-\n/gim, function(input){
-	input = input.trim().replace(/^(\w.+)\n--+-/gim, '$1')
-	return '<h2 id="'+ input.toLinkCase() +'">' + input + '</h2>'
-})
-.replace(/^#+\x20(.+)/gm, function(input) {
-	var number = 0
-	var output
-
-	function index(text) {
-
-		if(/^#+\x20/.test(text)){
-			number++
-			text = text.replace(/^#/, '')
-			index(text, number)
-		}else{
-			if (number == 0){
-				output = text
-				return;
-			}
-			const TITLE = text.replace(/^\x20/, '')
-			output = '<h'+number+' id="'+ TITLE.toLinkCase() +'">' + TITLE + '</h'+number+'>'
-		}
-	}
-	index(input)
-
-	return output
-})
-
-// IMG
-.replace(/\!\[([\x20-\x5A\x5C\x5E-\xFF]+)?\]\(([\x20-\x27\x2A-\xFF]+)(\s"[\w\d\s].+")?\)/g, '<img src="$2" alt="$1" title="$3">')
-
-// A
-.replace(
-	/\[([\x20-\x5A\x5C\x5E-\xFF]+)\]\(([\x21-\x27\x2A-\xFF]+)(\x20"[\w\d\s]+")?\)/g, function(input){
-		
-	var display = input.match(/\[(.+)\]/)[1]
-	var href   =   ' href="' + input.match(/\((.+)\)/)[1].replace(/\x20"(.+)"/, '') + '"'
-	var title  =  ' title="' + getTitle() + '"'
-	var target = ' target="'
-
-	if(/http/.test(href)){
-		target += '_blank"'
-	}else{
-		target += '_self"'
-		href = href.replace(/\.md/, '.html')
-	}
-	
-	function getTitle(){
-		if(/"(.+)"/.test(input)){
-			return input.match(/"(.+)"/)[1]
-		}
-		return ''
-	}
-
-	return '<a'+ href + title + target + '>' + display + '</a>'
-})
-
-// HR
-.replace(/(\n|^)--+-\n/g, '$1<hr>\n')
-
-// BR
-.replace(/(\n^\.\n|(\.|:|\!|\))\n\n(\w|\d|<|\*|`(``)?!))/g, '$2<br><br>$3')
-.replace(/(\\\n|\\n\w|(\.|:|\!|\))\n(\w|\d|<|\*\|`(``)?!))/g, '$2<br>$3')
-
-// PRE
-.replace(/```([\x09-\x5F\x61-\xFF]*)```/g, function(input){
-
-	var display = input
-		.replace(/```(\w+)?\n([\x09-\x5F\x61-\xFF]*)```/, '$2').parseHTML()
-
-	function getLang(){
-		if(/```(\w+)\n/.test(input)){
-			return input.match(/```(\w+)\n/)[1]
-		}
-		return ''
-	}
-
-	return '<pre class="' + getLang() + '">' + display + '</pre>'
-})
-
-// CODE
-.replace(/`([\x20-\x5F\x61-\xFF]+)`/g, function(input){
-	input = input.replace(/`([\x20-\x5F\x61-\xFF]+)`/, '$1').parseHTML()
-
-	return '<code>' + input + '</code>'
-})
-// SPAN
-.replace(/\[([\w\d\-\x20]+)\]\[([\x20-\x57\x59\x6B-\xFF]+)\]/gim, '<span class="$1">$2</span>')
-
-+ '<hr>\
-<p style="line-height: 22px;font-weight: 500;font-size: 14px; color:#8899a8;">\
-	CHM ' + LANG + ' ' + VERSION + ' - Mada with <3 by MatiDragon, Seemann & Yushae Raza.\
-</p>\
-<span id="alinks">Hoooola</span>'
-
-$('.markdown').innerHTML = $('#inputText').value 
+$('.markdown .cont').innerHTML = textToMarkdown($('#inputText').value)
 $('body').style['display'] = 'block'
 
 apply($('a'), function(e){
